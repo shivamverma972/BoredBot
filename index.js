c496bdc3d2c07a11fa5d563a21a4d5526c4d2fe2
Shivam Verma <74406787+shivamverma972@users.noreply.github.com>
2022-10-28 20:34:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+document.getElementById("get-activity").addEventListener("click",function(){
+    fetch("https://apis.scrimba.com/bored/api/activity")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("activity").textContent = data.activity
+            document.getElementById("title").textContent = "🦾HappyBot🦿"
+            document.body.classList.add("fun")
+            document.getElementById("get-activity").classList.add("button-color")
+        })
+})
